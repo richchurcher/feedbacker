15a73c83ecb06e3ac38759ee24d9ffb494a11d9f
Rich Churcher <rich.churcher@gmail.com>
2016-08-05 09:59:51 +1200

    Fix tests (avoid Ava JSX issues)

--- test/Students.test.js
+++ test/Students.test.js
@@ -2,14 +2,18 @@ import test from 'ava'
 import React from 'react'
 import {shallow} from 'enzyme'
 import Students from '../components/Students'
+import Student from '../components/Student'
 
 test('Students shallow correct', t => {
   const studentList = [{}, {}]
   const wrapper = shallow(<Students studentList={studentList} />)
-  t.is(wrapper.contains(<div>STUDENT LIST</div>), true)
+  const actual = wrapper.contains(<div></div>)
+  t.is(actual, true)
 })
 
-//test('Student shallow correct', t => {
-  //const wrapper = shallow(<Student />)
-  //t.is(wrapper.contains(<div>STUDENT DETAILS</div>), true)
-//})
+test('Student shallow correct', t => {
+  const student = {}
+  const wrapper = shallow(<Student row={student} />)
+  const actual = wrapper.contains(<div>STUDENT DETAILS</div>)
+  t.is(actual, true)
+})
